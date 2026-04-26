4fcb696d380bb38f6c199d9dc968f3ca88377512
vojta <vojtauhlik08@gmail.com>
2026-04-26 14:00:20 +0200

    Přidána kolize a skóre

--- .idea/src/CatchGame.java
+++ .idea/src/CatchGame.java
@@ -7,6 +7,7 @@ import java.util.*;
 public class CatchGame extends JPanel implements KeyListener {
 
     int playerX = 200;
+    int score = 0;
 
     ArrayList<Rectangle> objects = new ArrayList<>();
     Random rand = new Random();
@@ -18,20 +19,32 @@ public class CatchGame extends JPanel implements KeyListener {
 
         Timer timer = new Timer(30, e -> {
 
-
             if (rand.nextInt(20) == 0) {
                 objects.add(new Rectangle(rand.nextInt(380), 0, 20, 20));
             }
 
+            // pohyb
             for (Rectangle r : objects) {
                 r.y += 5;
             }
 
+
+            Rectangle player = new Rectangle(playerX, 550, 80, 10);
+
+            Iterator<Rectangle> it = objects.iterator();
+            while (it.hasNext()) {
+                Rectangle r = it.next();
+
+                if (r.intersects(player)) {
+                    score++;
+                    it.remove();
+                }
+            }
+
             repaint();
         });
-
-        timer.start();
     }
+
 
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -43,6 +56,8 @@ public class CatchGame extends JPanel implements KeyListener {
         for (Rectangle r : objects) {
             g.fillRect(r.x, r.y, 20, 20);
         }
+        g.drawString("Score: " + score, 10, 20);
+
     }
 
     public void keyPressed(KeyEvent e) {
@@ -52,6 +67,7 @@ public class CatchGame extends JPanel implements KeyListener {
 
     public void keyReleased(KeyEvent e) {}
     public void keyTyped(KeyEvent e) {}
+
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Catch Game");
