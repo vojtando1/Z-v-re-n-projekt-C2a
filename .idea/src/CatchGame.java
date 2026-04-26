442655df66c07d9ef22758f26a24983a476d2ddc
vojta <vojtauhlik08@gmail.com>
2026-04-26 10:23:02 +0200

    Inicializace projektu a vytvoření okna

--- .idea/src/CatchGame.java
+++ .idea/src/CatchGame.java
@@ -1,2 +1,10 @@
+import javax.swing.*;
+
 public class CatchGame {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Catch Game");
+        frame.setSize(400, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
 }
