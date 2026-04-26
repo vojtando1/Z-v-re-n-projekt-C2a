86bf52b92194049d821a1666dd9e32e41956c839
vojta <vojtauhlik08@gmail.com>
2026-04-26 11:11:09 +0200

    Přidány padající objekty

--- .idea/src/CatchGame.java
+++ .idea/src/CatchGame.java
@@ -1,25 +1,53 @@
 import javax.swing.*;
+import javax.swing.Timer;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.*;
 
 public class CatchGame extends JPanel implements KeyListener {
 
     int playerX = 200;
 
+    ArrayList<Rectangle> objects = new ArrayList<>();
+    Random rand = new Random();
+
     public CatchGame() {
         setFocusable(true);
         addKeyListener(this);
+
+
+        Timer timer = new Timer(30, e -> {
+
+
+            if (rand.nextInt(20) == 0) {
+                objects.add(new Rectangle(rand.nextInt(380), 0, 20, 20));
+            }
+
+            for (Rectangle r : objects) {
+                r.y += 5;
+            }
+
+            repaint();
+        });
+
+        timer.start();
     }
 
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
+
+        // hráč
         g.fillRect(playerX, 550, 80, 10);
+
+        // objekty
+        for (Rectangle r : objects) {
+            g.fillRect(r.x, r.y, 20, 20);
+        }
     }
 
     public void keyPressed(KeyEvent e) {
         if (e.getKeyCode() == KeyEvent.VK_LEFT) playerX -= 20;
         if (e.getKeyCode() == KeyEvent.VK_RIGHT) playerX += 20;
-        repaint();
     }
 
     public void keyReleased(KeyEvent e) {}
@@ -28,6 +56,7 @@ public class CatchGame extends JPanel implements KeyListener {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Catch Game");
         CatchGame game = new CatchGame();
+
         frame.add(game);
         frame.setSize(400, 600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
