866f11c4b21b7e0819725de65b9404ebf24bf820
vojta <vojtauhlik08@gmail.com>
2026-04-26 10:32:54 +0200

    Přidán hráč a ovládání šipkami

--- .idea/src/CatchGame.java
+++ .idea/src/CatchGame.java
@@ -1,8 +1,34 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
-public class CatchGame {
+public class CatchGame extends JPanel implements KeyListener {
+
+    int playerX = 200;
+
+    public CatchGame() {
+        setFocusable(true);
+        addKeyListener(this);
+    }
+
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.fillRect(playerX, 550, 80, 10);
+    }
+
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_LEFT) playerX -= 20;
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT) playerX += 20;
+        repaint();
+    }
+
+    public void keyReleased(KeyEvent e) {}
+    public void keyTyped(KeyEvent e) {}
+
     public static void main(String[] args) {
         JFrame frame = new JFrame("Catch Game");
+        CatchGame game = new CatchGame();
+        frame.add(game);
         frame.setSize(400, 600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
