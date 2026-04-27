ef97c6339a03956b82a5adfedd408ce8de7aa3e0
vojta <vojtauhlik08@gmail.com>
2026-04-27 16:02:18 +0200

    Přidána abstraktní třída GameObjec

--- .idea/src/GameObject.java
+++ .idea/src/GameObject.java
@@ -1,3 +1,13 @@
-public class GameObject {
+import java.awt.*;
 
+abstract class GameObject {
+    int x, y;
+    int speed = 5;
+
+    abstract void update();
+    abstract void draw(Graphics g);
+
+    Rectangle getBounds() {
+        return new Rectangle(x, y, 20, 20);
+    }
 }
